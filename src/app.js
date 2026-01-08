18cfab9e78c7daa8ab10e7b4e0d110b0efe43559
MirHussainJan <mirhussainjan10387@gmail.com>
2026-01-08 21:32:23 +0500

    Added the origins in CORS

--- src/app.js
+++ src/app.js
@@ -24,9 +24,10 @@ app.use(cookieParser());
 
 // allowed cors origins
 const allowedOrigins = [
-  "http://localhost:3000",
-  "https://www.sk-blackrock-financial.com",
-  "https://www.ectus-verwaltungs-ag.com"
+  "http://localhost:3000/",
+  "https://www.sk-blackrock-financial.com/",
+  "https://www.ectus-verwaltungs-ag.com/",
+  "https://rch-capital-holding.com/",
 ];
 
 app.use(
